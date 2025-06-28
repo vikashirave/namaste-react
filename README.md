3ee25964d3a28edad1ef08a0abeecbead68fb67b
vikashirave <vikashirave@gmail.com>
2025-06-28 15:05:52 +0530

    episode 05 lets get hooked used usestate.

--- README.md
+++ README.md
@@ -17,3 +17,33 @@
 - HTTPs
 - Tree Shaking - remove unused code
 - Diffrent Dev and Prod bundles
+
+
+// Build plan for app.
+
+- Header
+- - logo
+- - Nav Items
+- Body
+- - Search
+- - RestaurantContainer
+- - RestaurantCard
+- - Name of Res, star rating, cuisine, etc
+- Footer
+- - Copyright
+- - Links
+- - Address
+- - Contact
+ */
+
+
+default export/import
+default export component
+import from "path"
+
+named export import
+export const component;
+import {component} from "path"
+
+
+useState
---
+++ src/components/Body.js
@@ -0,0 +1,31 @@
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import resList from "../utils/mockData";
+
+const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.2
+            );
+            setListOfRestaurants(filteredRestaurants);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
