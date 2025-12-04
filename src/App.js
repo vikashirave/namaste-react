18d1b924051a6ca0fa8c555f4a3731871db6b5b9
vikas <vikashirave@gmail.com>
2025-12-04 17:22:16 +0530

    lazy loading added to component..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+// chunking
+// Code Splitting
+// Dynamic Bundling
+// lazy loading
+// on demand laoding
+
+const Grocery = lazy(()=> import("./components/Grocery"));
+const About = lazy(()=>import("./components/About"));
 
 AppLayout = () => {
   return (
@@ -28,11 +36,15 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: <Suspense fallback={<h1>Loading....</h1>}><About /></Suspense>,
       },
       {
         path: "/contact",
         element: <Contact />,
+      },
+      {
+        path: "/grocery",
+        element: <Suspense fallback={<h1>Loading....</h1>}><Grocery /></Suspense>,
       },
       {
         path: "/restaurant/:resId",
